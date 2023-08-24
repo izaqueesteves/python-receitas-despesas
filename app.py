c37058807ee0a01fc9617e86684c270af649821b
Aluno Lab 135 <lab135@lab.senai>
2023-08-24 14:30:09 -0300

    Versão 3 mostrando saldo após IF

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ receitas=int(input("Digite o total de despesas: "))
 despesas=int(input("Digite o total de receitas: "))
 saldo=receitas-despesas
 if(saldo>0):
-    print("Saldo positivo")
+    print("Saldo positivo: ",saldo)
 else:
-    print("Saldo negativo ou zero")
+    print("Saldo negativo ou zero: ",saldo)
